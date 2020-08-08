c958b92cb7739a7baeb9d761f5bbc9196c80affd
asimali7 <33003365+asimali7@users.noreply.github.com>
2020-08-08 22:00:06 +0500

    Updates

---
+++ precache-manifest.4467252d76f702511c9932acd0da7793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379eefe123d858affef158d2baa897f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e9ca2d7d41d58829d01",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "841c684d008b31734ac1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e9ca2d7d41d58829d01",
+    "url": "/static/js/2.c1c186cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1c186cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841c684d008b31734ac1",
+    "url": "/static/js/main.37b1380d.chunk.js"
+  },
+  {
+    "revision": "3cc9d6562f64fc197f20",
+    "url": "/static/js/runtime-main.fc30dc24.js"
+  }
+]);
